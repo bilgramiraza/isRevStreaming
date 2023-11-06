e3afd760eb3a32ee816d2e6769fc1517f23172dc
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-06 15:22:55 +0530

    Added More Details to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import useStreamStatus from "./components/useStreamStatus";
 import './app.css';
+import offline from './assets/offlineGengar.jpeg';
+import online from './assets/onlineGengar.jpg';
 
 function App() {
   const { isOnline, error, loading } = useStreamStatus();
-  let contentDiv;
+  let mainContentDiv, imageDiv;
   if(loading){
-    contentDiv = <span className="loader"></span>;
+    mainContentDiv = <span className="loader"></span>;
   }else if(error){
-    contentDiv = <p>{"Ope, Something Messed Up, Either wait a few Mins and if that doesn't help Contact Me"}</p>;
+    mainContentDiv = <p>{"Ope, Something Messed Up, Either wait a few Mins and try again and if that doesn't help Contact Me"}</p>;
   }else{
-    contentDiv = <h2>{isOnline?'HE IS':"HE ISN'T"}</h2>;
+    mainContentDiv = <h2>{isOnline?'HE IS':"HE ISN'T"}</h2>;
+    imageDiv = <img src={isOnline?online:offline} alt={isOnline?'happyGengarImage':'sadGengarImage'} width={300} height={200}/>
   }
   return (
     <div>
       <h1>Is Reverse094 Online?</h1>
-      {contentDiv}
+      {mainContentDiv}
+      {isOnline && <a href="https://www.twitch.tv/reverse094" target="_blank" rel="noreferrer">Go There</a>}
+      {imageDiv}
+      <h4>While you are here, check out his <a href="https://twitter.com/reverse094" target="_blank" rel="noreferrer">Twitter</a></h4>
     </div>
   );
 }

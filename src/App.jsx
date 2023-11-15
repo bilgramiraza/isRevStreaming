6c93083efa9bf117cef42e145be2b3dbc8721505
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-15 11:40:49 +0530

    Setting up UI Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import useStreamStatus from "./components/useStreamStatus";
 import './app.css';
 import offline from './assets/offlineGengar.jpeg';
 import online from './assets/onlineGengar.jpg';
+import TwitchDiv from "./components/TwitchDiv";
 
 function App() {
-  const { isOnline, error, loading } = useStreamStatus();
+  const { isOnline, data, error, loading } = useStreamStatus();
   let mainContentDiv, imageDiv;
   if(loading){
     mainContentDiv = <span className="loader"></span>;
@@ -12,15 +13,18 @@ function App() {
     mainContentDiv = <p>{"Ope, Something Messed Up, Either wait a few Mins and try again and if that doesn't help Contact Me"}</p>;
   }else{
     mainContentDiv = <h2>{isOnline?'HE IS':"HE ISN'T"}</h2>;
-    imageDiv = <img src={isOnline?online:offline} alt={isOnline?'happyGengarImage':'sadGengarImage'} width={300} height={200}/>
+    imageDiv = <img src={isOnline ? online : offline } alt={isOnline?'happyGengarImage':'sadGengarImage'} width={300} height={200}/>
   }
   return (
-    <div>
-      <h1>Is Reverse094 Online?</h1>
-      {mainContentDiv}
-      {isOnline && <h2><a href="https://www.twitch.tv/reverse094" target="_blank" rel="noreferrer">Go There</a></h2>}
-      {imageDiv}
-      <h4>While you are here, check out his <a href="https://twitter.com/reverse094" target="_blank" rel="noreferrer">Twitter</a></h4>
+    <div className="main">
+      <div className="statusDiv">
+        <h1>Is Reverse094 Online?</h1>
+        {mainContentDiv}
+        {isOnline && <h2><a href="https://www.twitch.tv/reverse094" target="_blank" rel="noreferrer">Go There</a></h2>}
+        {imageDiv}
+        <h4>While you are here, check out his <a href="https://twitter.com/reverse094" target="_blank" rel="noreferrer">Twitter</a></h4>
+      </div>
+      {isOnline && <TwitchDiv data={data}/>}
     </div>
   );
 }

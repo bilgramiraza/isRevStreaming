08415fd20242162b50924c6bc77d07482e8c9eb6
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-15 13:06:52 +0530

    Cleanedup UI

--- src/components/TwitchDiv.jsx
+++ src/components/TwitchDiv.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import './twitch.css';
+import { formatDistance, parseISO } from 'date-fns';
 
 const TwitchDiv = ({ data }) => {
   const { 
@@ -9,26 +10,33 @@ const TwitchDiv = ({ data }) => {
     game, 
     tags,
     viewerCount, 
-    startedAt } = data;
+    startedAt,
+    updatedAt
+  } = data;
+  const streamTimer = formatDistance(parseISO(startedAt), parseISO(updatedAt));
   const tagDivs = tags?.map(tag=><span className='tag' key={ tag }>{ tag }</span>);
   return (
     <div className='twitchDiv'>
       <div>
-        <h2>Stream Preview</h2>
+        <h2 className='twitchDivTitle'>Stream Preview</h2>
       </div>
       <div>
-        <img src={ latestThumbnail } height={480} width={640}/>
+        <img src={ latestThumbnail } alt='Latest Thumbnail' height={480} width={640}/>
       </div>
       <div className='twitchData'>
-        <div className='leftPanel'>
-          <h3 className='topLevelText'>{ userName }</h3>
+        <div className='topPanel'>
+          <div className='leftPanel'>
+            <h3 className='topLevelText'><a href='https://www.twitch.tv/reverse094'>{ userName }</a></h3>
+          </div>
+          <div className='rightPanel'>
+            <h4 className='lowLevelText'>Viewer Count: { viewerCount }</h4>
+            <h4 className='lowLevelText'>Stream Time: { streamTimer }</h4>
+          </div>
+        </div>
+        <div className='bottomPanel'>
           <h4 className='midLevelText'>Title: { streamTitle }</h4>
           <h5 className='lowLevelText'>Category: { game }</h5>
           <p className='tagsDiv'>Tags: { tagDivs }</p>
-        </div>
-        <div className='rightPanel'>
-          <h4 className='lowLevelText'>Viewer Count: { viewerCount }</h4>
-          <h4 className='lowLevelText'>Stream Time: { startedAt }</h4>
         </div>
       </div>
     </div>

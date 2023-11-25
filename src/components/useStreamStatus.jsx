1269062c70d9a6e62380a5bde5d5ebf503c5861a
bilgramiraza <bilgramiraza@yahoo.com>
2023-11-25 17:19:39 +0530

    Added an Alternate API incase Main Server takes too long to load

--- src/components/useStreamStatus.jsx
+++ src/components/useStreamStatus.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 
-const getData = async (handleIsOnline, handleData, handleError, handleLoading) => {
+const getStreamData = async (handleIsOnline, handleData, handleError, handleLoading, altSourceTimer) => {
   try{
+    handleLoading(true);
     const response = await fetch(`${import.meta.env.VITE_PRIMARY_API_URI}/${import.meta.env.VITE_STREAMER_USERNAME}`);
+    clearTimeout(altSourceTimer);
     const data = await response.json();
     handleIsOnline(data.isLive);
     handleData({
@@ -23,6 +25,20 @@ const getData = async (handleIsOnline, handleData, handleError, handleLoading) =
   }
 }
 
+const getOnlineStatus = async (handleIsOnline, handleData, handleError, handleLoading)=>{
+  try{
+    handleLoading(true);
+    const response = await fetch(`${import.meta.env.VITE_SECONDARY_API_URI}/${import.meta.env.VITE_STREAMER_USERNAME}`);
+    const data = await response.text();
+    handleIsOnline(!data.includes('is offline'));
+    handleData({});
+  }catch(err){
+    handleError(err);
+  }finally{
+    handleLoading(false);
+  }
+}
+
 export default function useStreamStatus(){
   const [ data, setData ] = useState({});
   const [ isOnline, setIsOnline ] = useState(false);
@@ -30,7 +46,10 @@ export default function useStreamStatus(){
   const [ loading, setLoading ] = useState(true);
 
   useEffect(()=>{
-    getData(setIsOnline, setData, setError, setLoading);
+    const altSourceTimer = setTimeout(()=>
+      getOnlineStatus(setIsOnline, setData, setError, setLoading)
+      ,1000);
+    getStreamData(setIsOnline, setData, setError, setLoading, altSourceTimer);
   },[]);
   return { isOnline, data, error, loading };
 }

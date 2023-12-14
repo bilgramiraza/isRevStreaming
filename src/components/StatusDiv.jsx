d3e5794dbd04b51178f8804fb0a1d42751bdce7f
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-14 19:35:12 +0530

    Fixed CSS Files and their Imports per react guidelines

--- src/components/StatusDiv.jsx
+++ src/components/StatusDiv.jsx
@@ -2,26 +2,26 @@ import offline from '../assets/offlineGengar.jpeg';
 import online from '../assets/onlineGengar.jpg';
 import { TwitchSvg, TwitterSvg } from "../assets/svg";
 import PropTypes from 'prop-types';
-import './status.css';
+import style from './status.module.css';
 
 const StatusDiv= ({ isOnline })=>{
   let mainContentDiv, imageDiv;
   if(isOnline){
-    mainContentDiv = <h2 className="pageTitleReplyYes">HE IS</h2>;
-    imageDiv = <img className="statusImage" src={ online } alt={ 'happyGengarImage' }/>
+    mainContentDiv = <h2 className={ style.pageTitleReplyYes }>HE IS</h2>;
+    imageDiv = <img className={ style.statusImage } src={ online } alt={ 'happyGengarImage' }/>
   }else{
-    mainContentDiv = <h2 className="pageTitleReplyNo">{"HE ISN'T"}</h2>;
-    imageDiv = <img className="statusImage" src={ offline } alt={ 'sadGengarImage' }/>
+    mainContentDiv = <h2 className={ style.pageTitleReplyNo }>{"HE ISN'T"}</h2>;
+    imageDiv = <img className={ style.statusImage } src={ offline } alt={ 'sadGengarImage' }/>
   }
   
   return (
     <>
-      <div className="statusTextDiv">
+      <div className={ style.statusTextDiv }>
         {mainContentDiv}
-        {isOnline && <a className="twitchBtn" href={`https://www.twitch.tv/${import.meta.env.VITE_STREAMER_USERNAME}`} target="_blank" rel="noreferrer">Go There <TwitchSvg /></a>}
+        {isOnline && <a className={ style.twitchBtn } href={`https://www.twitch.tv/${import.meta.env.VITE_STREAMER_USERNAME}`} target="_blank" rel="noreferrer">Go There <TwitchSvg /></a>}
       </div>
       {imageDiv}
-      <h4 className="socialsText">While you are here, check out his <a className="twitterBtn" href="https://twitter.com/reverse094" target="_blank" rel="noreferrer">Twitter <TwitterSvg /></a></h4>
+      <h4 className={ style.socialsText }>While you are here, check out his <a className={ style.twitterBtn } href="https://twitter.com/reverse094" target="_blank" rel="noreferrer">Twitter <TwitterSvg /></a></h4>
     </>
   );
 }
